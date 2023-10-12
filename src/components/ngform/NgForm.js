5a06f7350f66a59a2f729fb92027a4f280153265
Lloyd Semblante <92197616+lloydshine@users.noreply.github.com>
2023-10-12 05:58:35 +0000

    fix

--- src/components/ngform/NgForm.js
+++ src/components/ngform/NgForm.js
@@ -2,10 +2,10 @@ import './ngform.css';
 
 export default function NgForm() {
   return (
-    <form name='message v1' method="post" onSubmit="submit" data-netlify="true">
-      <input type='hidden' name='form-name' value='message v1' />
-      <label htmlFor='message'>Message:</label>
-      <textarea name='message' id='message' rows="7" cols="40" placeholder='Enter Message'></textarea>
+    <form name='message' method="post">
+      <input type='hidden' name='form-name' value='message' />
+      <label htmlFor='secretmessage'>Message:</label>
+      <textarea name='secretmessage' id='secretmessage' rows="7" cols="40" placeholder='Enter Message'></textarea>
       <button type='submit'>Send</button>
     </form>
   )
