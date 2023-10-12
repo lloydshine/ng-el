01b78c0a69669879f1b788ae28d7c2bc55e15897
Lloyd Semblante <92197616+lloydshine@users.noreply.github.com>
2023-10-12 05:29:32 +0000

    goods

--- src/components/ngform/NgForm.js
+++ src/components/ngform/NgForm.js
@@ -2,7 +2,7 @@ import './ngform.css';
 
 export default function NgForm() {
   return (
-    <form>
+    <form netlify="true">
       <label htmlFor='message'>Message:</label>
       <textarea name='message' id='message' rows="7" cols="40" placeholder='Enter Message'></textarea>
       <button type='submit'>Send</button>
